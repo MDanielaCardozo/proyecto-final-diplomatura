b95866bbee715818e80918f213432d2c239be723
Daniela Cardozo <mdanielacardozo@gmail.com>
2025-01-08 21:26:06 -0300

    add home css

--- src/components/views/Home/Carousel/CarouselHome.jsx
+++ src/components/views/Home/Carousel/CarouselHome.jsx
@@ -1,35 +1,38 @@
 import { Carousel } from "react-bootstrap";
+import CarouselUno from "../../../../assets/img/carousel-uno.jpg";
+import CarouselDos from "../../../../assets/img/carousel-dos.jpg";
+import CarouselTres from "../../../../assets/img/carousel-tres.jpg";
 
 function CarouselHome() {
-    return (
-        <div>
-            <Carousel>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Second slide" /> */}
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-        </div>
-    );
+  return (
+    <div className="container py-5">
+      <Carousel className="carousel-fade"> 
+        <Carousel.Item>
+          <img src={CarouselUno} alt="Portada carousel uno" className="d-block w-100" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Step into the Future of Lighting with Bright Innovations</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={CarouselDos} alt="Portada carousel dos" className="w-100" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={CarouselTres} alt="Portada carousel tres" className="w-100" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
 
 export default CarouselHome;
--- src/components/views/Home/Home.jsx
+++ src/components/views/Home/Home.jsx
@@ -1,9 +1,21 @@
 import CarouselHome from "./Carousel/CarouselHome";
+import { Container, Row, Col } from "react-bootstrap";
+import CardProduct from "./CardProduct/CardProduct";
 
 function Home() {
     return (
-        <div>
+        <div className="bg-dark">
             <CarouselHome></CarouselHome>
+            <Container className="py-5 text-white ">
+                <h1 className="py-5">Products</h1>
+                <hr className="p-5"/>
+
+                <Row className="py-5">
+                    <Col>
+                        <CardProduct></CardProduct>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
